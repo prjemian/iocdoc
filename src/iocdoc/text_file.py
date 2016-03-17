6090d0a9c5d16880bb0500254fdc0e3445602b08
jemian <prjemian@gmail.com>
2016-03-17 01:20:46 -0500

    a diagnostic

--- src/iocdoc/text_file.py
+++ src/iocdoc/text_file.py
@@ -120,6 +120,7 @@ class _TextFile(object):
     def __init__(self, filename):
         self.filename = filename
         if not os.path.exists(filename):
+            pwd = os.getcwd()
             raise FileNotFound(filename)
 
         self.absolute_filename = os.path.abspath(filename)
