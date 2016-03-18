d25b9b25e6c758817aa81fde423aede6000a292d
jemian <prjemian@gmail.com>
2016-03-18 10:50:24 -0500

    copy into main code

--- src/iocdoc/token_support.py
+++ src/iocdoc/token_support.py
@@ -346,6 +346,128 @@ def token_key(tkn):
     return m
 
 
+def parse_bracketed_macro_definitions(tokenLog):
+    '''
+    walk through a bracketed string, keeping track of delimiters
+    
+    verify we start on an opening delimiter
+    '''
+    analysis = _find_sections(tokenLog)
+
+    token_dividers = [analysis['start'], analysis['end']]
+    for key in 'commas equals'.split():
+        token_dividers += analysis[key]
+    token_dividers.sort()
+    
+    if len(analysis['commas']) == 0 and len(analysis['equals']) == 0:
+        # No delimiters found: either no macro, 1 macro, or space-delimited.
+        # Cannot become a dict since no "=" were found.
+        # Look at all tokens between the enclosure, 
+        # accumulate contiguous text,
+        # break on non-contiguous boundaries
+        # Note: makes no assumption about all on one line.
+        s, f = token_dividers
+        l, c = tokenLog.get(s)['end']
+        text = ''
+        parts = []
+        for i in range(s+1, f):
+            tok = tokenLog.get(i)
+            if tok['start'][1] != c or tok['start'][0] != l:
+                if len(text) > 0:
+                    parts.append(text)
+                text = tok['tokStr']
+            else:
+                text += tok['tokStr']
+            l, c = tokenLog.get(i)['end']
+        if len(text) > 0:
+            parts.append(text)
+        return parts
+
+    text_list = []
+    for index, key in enumerate(token_dividers[0:-1]):
+        s = key+1
+        f = token_dividers[index+1]
+        text_list.append( _rebuild_text([tokenLog.get(_) for _ in range(s, f)]) )
+    
+    if len(analysis['commas']) > len(analysis['equals']):
+        return text_list
+    else:
+        # tricky: http://stackoverflow.com/questions/6900955/python-convert-list-to-dictionary
+        # if text_list = ['a', 'b', 'c', 'd']
+        # this returns dict(a='b', c='d')
+        return dict(zip(text_list[::2], text_list[1::2]))
+
+
+def _find_sections(tokenLog):
+    '''
+    locate the tokens that divide this sequence into sections
+    
+    The overall section is delimited by {} or ().
+    Internally, the delimiters are , or =.
+    All the rest (that is not a comment) is string content to be kept.
+    Return the sections as a a dictionary with these members:
+    
+    * 'open': token number for the opening symbol
+    * 'commas': list of token numbers for comma delimiters
+    * 'equals': list of token numbers for equal sign delimiters
+    * 'close': token number for the matching closing symbol
+    '''
+    terminator = {
+                  '{': 'OP }',
+                  '(': 'OP )',
+                  }
+    
+    tok = tokenLog.getCurrentToken()
+    c = tok['tokStr']
+    if c not in terminator:
+        l, c = tok['start']
+        msg = '(%d,%d) ' % (l, c+1)
+        msg += 'token stream not starting with "(" or "{"'
+        raise KeyError, msg
+
+    pt_start = tokenLog.token_pointer
+    tk_start = token_key(tok)
+    tk_end = terminator[c]
+    depth = 1
+    tok = tokenLog.nextActionable()
+    commas = []
+    equals = []
+    while depth > 0:
+        tk = token_key(tok)
+        if tk == 'OP ,' and depth == 1:
+            commas.append(tokenLog.token_pointer)
+        elif tk == 'OP =' and depth == 1:
+            equals.append(tokenLog.token_pointer)
+        elif tk == tk_start:
+            depth += 1
+        elif tk == tk_end:
+            depth -= 1
+            if depth == 0:
+                pt_end = tokenLog.token_pointer
+        tok = tokenLog.nextActionable()
+    
+    return dict(
+                start = pt_start,
+                end = pt_end,
+                commas = commas,
+                equals = equals,
+                )
+
+
+def _rebuild_text(token_list):
+    '''
+    reconstruct the text from the list of tokens
+    '''
+    text = ''
+    for tok in token_list:
+        # Q: What if tok['tokName'] is a COMMENT or other undesirable?
+        # A: not common in macro definitions, fix code if this is seen
+        # Q: what about line number or column number gaps between tokens?
+        # A: addressed above, do not mix comma delimited and whitespace delimited
+        text += tok['tokStr']
+    return text
+
+
 ######################################################################
 
 
