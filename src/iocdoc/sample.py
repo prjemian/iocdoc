02c2ca938a9c9fb6e95827cbb78f6209c6d54c94
jemian <prjemian@gmail.com>
2016-03-18 10:27:49 -0500

    will not try to handle most uncommon case now, not sure how to raise exception for it either

--- src/iocdoc/sample.py
+++ src/iocdoc/sample.py
@@ -1,6 +1,12 @@
 
 '''
 develop a better token stream parser for macro expressions
+
+Cannot correctly handle this uncommon definition::
+
+    $(abcd=$(a)$(b)$(c)$(d),a=A,b=B,c=C,d=D)
+
+eventually, this will go into the "macros" module
 '''
 
 import os
@@ -12,6 +18,92 @@ def parse_bracketed(tokenLog):
     walk through a bracketed string, keeping track of delimiters
     
     verify we start on an opening delimiter
+    '''
+    #print '-'*20
+    analysis = _find_sections(tokenLog)
+
+    token_dividers = [analysis['start'], analysis['end']]
+    for key in 'commas equals'.split():
+        token_dividers += analysis[key]
+    token_dividers.sort()
+    #print token_dividers
+    
+    if len(analysis['commas']) == 0 and len(analysis['equals']) == 0:
+        # No delimiters found: either no macro, 1 macro, or space-delimited.
+        # Cannot become a dict since no "=" were found.
+        # Look at all tokens between the enclosure, 
+        # accumulate contiguous text,
+        # break on non-contiguous boundaries
+        # Note: makes no assumption about all on one line.
+        s, f = token_dividers
+        l, c = tokenLog.get(s)['end']
+        text = ''
+        parts = []
+        for i in range(s+1, f):
+            tok = tokenLog.get(i)
+            if tok['start'][1] != c or tok['start'][0] != l:
+                if len(text) > 0:
+                    parts.append(text)
+                text = tok['tokStr']
+            else:
+                text += tok['tokStr']
+            l, c = tokenLog.get(i)['end']
+        if len(text) > 0:
+            parts.append(text)
+        return parts
+
+    text_list = []
+    for index, key in enumerate(token_dividers[0:-1]):
+        s = key+1
+        f = token_dividers[index+1]
+        text_list.append( _rebuild_text([tokenLog.get(_) for _ in range(s, f)]) )
+    
+    if len(analysis['commas']) > len(analysis['equals']):
+        return text_list
+    else:
+        # tricky: http://stackoverflow.com/questions/6900955/python-convert-list-to-dictionary
+        # if text_list = ['a', 'b', 'c', 'd']
+        # this returns dict(a='b', c='d')
+        return dict(zip(text_list[::2], text_list[1::2]))
+
+
+def print_token(tok):
+    print '(%d,%d)-(%d,%d) %s: %s' % (
+         tok['start'][0],
+         tok['start'][1],
+         tok['end'][0],
+         tok['end'][1],
+         tok['tokName'],
+         tok['tokStr'],
+     )
+
+
+def _rebuild_text(token_list):
+    '''
+    reconstruct the text from the list of tokens
+    '''
+    text = ''
+    for tok in token_list:
+        # TODO: what if tok['tokName'] is a COMMENT or other undesirable?
+        # TODO: what about line number or column number gaps between tokens?
+        text += tok['tokStr']
+        # print_token(tok)
+    return text
+
+
+def _find_sections(tokenLog):
+    '''
+    locate the tokens that divide this sequence into sections
+    
+    The overall section is delimited by {} or ().
+    Internally, the delimiters are , or =.
+    All the rest (that is not a comment) is string content to be kept.
+    Return the sections as a a dictionary with these members:
+    
+    * 'open': token number for the opening symbol
+    * 'commas': list of token numbers for comma delimiters
+    * 'equals': list of token numbers for equal sign delimiters
+    * 'close': token number for the matching closing symbol
     '''
     terminator = {
                   '{': 'OP }',
@@ -35,9 +127,9 @@ def parse_bracketed(tokenLog):
     equals = []
     while depth > 0:
         tk = token_key(tok)
-        if tk == 'OP ,':
+        if tk == 'OP ,' and depth == 1:
             commas.append(tokenLog.token_pointer)
-        elif tk == 'OP =':
+        elif tk == 'OP =' and depth == 1:
             equals.append(tokenLog.token_pointer)
         elif tk == tk_start:
             depth += 1
@@ -46,12 +138,20 @@ def parse_bracketed(tokenLog):
             if depth == 0:
                 pt_end = tokenLog.token_pointer
         tok = tokenLog.nextActionable()
-    print [tokenLog.get(_)['start'][1]+1 for _ in commas],
-    print [tokenLog.get(_)['start'][1]+1 for _ in equals]
-    # TODO:
+    
+    return dict(
+                start = pt_start,
+                end = pt_end,
+                commas = commas,
+                equals = equals,
+                )
+
+def showSeq(token_list):
+    for tok in token_list:
+        print_token(tok)
 
 
-def parse_definition_block(tokenLog):
+def parse_macro_definition_block(tokenLog):
     tok = tokenLog.getCurrentToken()
     while token_key(tok) != 'NAME file':
         tok = tokenLog.nextActionable()
@@ -60,13 +160,22 @@ def parse_definition_block(tokenLog):
     tok = tokenLog.nextActionable()   # filename
     tok = tokenLog.nextActionable()   # {
     tok = tokenLog.nextActionable()   # { | pattern
+
+    definitions = []
+    labels = []
     if token_key(tok) == 'NAME pattern':
         tok = tokenLog.nextActionable()     # {
-        parse_bracketed(tokenLog)           # labels
+        labels = parse_bracketed(tokenLog)           # labels
         tok = tokenLog.getCurrentToken()
     while token_key(tok) != 'OP }':
-        parse_bracketed(tokenLog)           # (labels): values
+        thing = parse_bracketed(tokenLog)           # (labels): values
+        if isinstance(thing, list):
+            env = dict(zip(labels, thing))
+        else:
+            env = thing
         tok = tokenLog.getCurrentToken()
+        definitions.append(env)
+        print env
 
 
 def main():
@@ -78,7 +187,7 @@ def main():
     tok = tokenLog.nextActionable()
 
     while tok is not None and tok['tokName'] != 'ENDMARKER':
-        parse_definition_block(tokenLog)
+        parse_macro_definition_block(tokenLog)
         tok = tokenLog.getCurrentToken()
 
 if __name__ == '__main__':
