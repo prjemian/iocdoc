a18e902e44a95d60dd38b9d54c0c574a5235a8eb
prjemian <prjemian@gmail.com>
2016-03-21 12:58:23 -0500

    start parsing the st.cmd file

--- src/iocdoc/command_file.py
+++ src/iocdoc/command_file.py
@@ -6,6 +6,13 @@ import os
 import token
 import tokenize
 
+import database
+import macros
+import template
+import text_file
+from token_support import token_key, TokenLog, parse_bracketed_macro_definitions
+from utils import logMessage, FileRef
+
 
 '''
 TODO: (from topdoc.CmdReader()): refactor this code to:
@@ -26,22 +33,63 @@ class CommandFile(object):
     '''
 
 
-    def __init__(self, parent, fobject, env={}):
+    def __init__(self, parent, filename, env={}):
         self.parent = parent
-        self.fobject = fobject
+        self.filename = filename
+        self.reference = None
 
         self.env = dict(env.items())
         self.symbols = {}
         self.commands = []
-    
-    def expand_macros(self):
-        '''expand all known macros'''
-        raise NotImplementedError()
+        self.ref_list = []
+
+        # filename is a relative or absolute path to command file, no macros in the name
+        self.source = text_file.read(filename)
+
+        self.parse()
     
     def parse(self):
         '''analyze this command file'''
-        raise NotImplementedError()
+        tokenLog = TokenLog()
+        tokenLog.processFile(self.filename)
+        tok = tokenLog.nextActionable()
+        actions = {
+                   'NAME putenv XX': self._parse_putenv,
+                   }
+        while tok is not None:
+            tk = token_key(tok)
+            if tok['tokName'] in ('NAME',):
+                ref = FileRef(self.filename, tok['start'][0], tok['start'][1], tok['tokStr'])
+                self.ref_list.append(ref)
+            if tk in actions:
+                actions[tk](tokenLog)
+            tok = tokenLog.nextActionable()
      
     def report(self):
         '''describe what was discovered'''
         raise NotImplementedError()
+
+    def _parse_putenv(self, tokenLog):
+        raise NotImplementedError
+
+
+def main():
+    cmd = {}
+    testfiles = []
+    testfiles.append(os.path.join('.', 'testfiles', 'iocBoot', 'ioc495idc', 'st.cmd'))
+    for tf in testfiles:
+        try:
+            cmd[tf] = CommandFile(None, tf)
+        except text_file.FileNotFound, _exc:
+            print 'file not found: ' + tf
+            continue
+        except NotImplementedError, _exc:
+            print 'Not implemented yet: ' + str(_exc)
+            continue
+        print cmd[tf]
+        for ref in cmd[tf].ref_list:
+            print str(ref)
+
+
+if __name__ == '__main__':
+    main()
