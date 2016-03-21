413a607c2ce40863f31d0c5c8cba21524a3fdf9b
jemian <prjemian@gmail.com>
2016-03-21 18:53:29 -0500

    use the testfiles

--- src/iocdoc/testfiles/iocBoot/ioc495idc/st.cmd
+++ src/iocdoc/testfiles/iocBoot/ioc495idc/st.cmd
@@ -13,6 +13,8 @@ cd ""
 < ../nfsCommands
 < cdCommands
 
+putenv("TOP=./testfiles")
+
 # How to set and get the clock rate (in Hz.  Default is 60 Hz)
 #sysClkRateSet(100)
 #sysClkRateGet()
@@ -22,6 +24,7 @@ cd topbin
 
 # If the VxWorks kernel was built using the project facility, the following must
 # be added before any C++ code is loaded (see SPR #28980).
+sysCplusEnable=1
 sysCplusEnable=1
 
 ### Load synApps EPICS software
@@ -62,17 +65,17 @@ ioc495idcVX_registerRecordDeviceDriver(pdbbase)
 
 # IAI X-Sel support - ONLY UNCOMMENT WHEN DET EBRICK IS OFFLINE
 < cmds/IAI.cmd
-dbLoadRecords("$(TOP)/495idcApp/Db/asynRecordAliases.db","P=495idc:,R=port09,PA=495idcDET:,RA=port09")
-dbLoadRecords("$(TOP)/495idcApp/Db/iai_controlsAliases.db","P=495idc:,R=IAI:,PA=495idcDET:,RA=IAI:")
-dbLoadRecords("$(TOP)/495idcApp/Db/detBaseAliases.db","P=495idc:,H=base:,PA=495idcDET:,HA=base:")
-dbLoadRecords("$(TOP)/495idcApp/Db/detRobotAliases.db","P=495idc:,H=robot:,PA=495idcDET:,HA=robot:")
+dbLoadRecords("$(TOP)/databases/asynRecordAliases.db","P=495idc:,R=port09,PA=495idcDET:,RA=port09")
+#dbLoadRecords("$(TOP)/databases/iai_controlsAliases.db","P=495idc:,R=IAI:,PA=495idcDET:,RA=IAI:")
+#dbLoadRecords("$(TOP)/databases/detBaseAliases.db","P=495idc:,H=base:,PA=495idcDET:,HA=base:")
+#dbLoadRecords("$(TOP)/databases/detRobotAliases.db","P=495idc:,H=robot:,PA=495idcDET:,HA=robot:")
 
 # Motors
 dbLoadTemplate("templates/motor.substitutions")
 iocshCmd "epicsThreadSleep(5.0)"
 dbLoadTemplate("templates/softMotor.substitutions")
 # Create PV aliases so old medm screens can be used
-dbLoadRecords("$(TOP)/495idcApp/Db/motorAliases.db")
+#dbLoadRecords("$(TOP)/databases/motorAliases.db")
 
 # Xradia
 < cmds/xradia.cmd
@@ -81,7 +84,7 @@ dbLoadRecords("$(TOP)/495idcApp/Db/motorAliases.db")
 # Slits
 < cmds/slits.cmd
 # Create PV aliases so old medm screens can be used
-dbLoadRecords("$(TOP)/495idcApp/Db/slitAliases.db")
+#dbLoadRecords("$(TOP)/databases/slitAliases.db")
 
 # pf4 filters
 dbLoadTemplate("templates/filter.substitutions")
@@ -89,8 +92,8 @@ dbLoadTemplate("templates/filter.substitutions")
 # digitelPump support - ONLY UNCOMMENT WHEN NES EBRICK IS OFFLINE
 < cmds/digitelPump.cmd
 # Create PV aliases so old medm screens can be used
-dbLoadRecords("$(TOP)/495idcApp/Db/digitelPumpAliases.db","P=495idc:,PUMP=IP01,PA=495idcNES:,PUMPA=IP01")
-dbLoadRecords("$(TOP)/495idcApp/Db/digitelPumpAliases.db","P=495idc:,PUMP=IP02,PA=495idcNES:,PUMPA=IP02")
+#dbLoadRecords("$(TOP)/databases/digitelPumpAliases.db","P=495idc:,PUMP=IP01,PA=495idcNES:,PUMPA=IP01")
+#dbLoadRecords("$(TOP)/databases/digitelPumpAliases.db","P=495idc:,PUMP=IP02,PA=495idcNES:,PUMPA=IP02")
 
 # table records
 dbLoadTemplate("templates/table.substitutions")
@@ -98,27 +101,27 @@ dbLoadTemplate("templates/table.substitutions")
 # Load database records for Femto amplifiers/photodiode
 #!dbLoadRecords("$(STD)/stdApp/Db/femto.db","P=495idc:,H=fem01:,F=seq01:")
 # Create PV aliases so old medm screens can be used
-#!dbLoadRecords("$(TOP)/495idcApp/Db/femtoAliases.db","P=495idc:,H=fem01:,F=seq01:,PA=495idcnpi:,HA=fem01:,FA=seq01:")
+#!dbLoadRecords("$(TOP)/databases/femtoAliases.db","P=495idc:,H=fem01:,F=seq01:,PA=495idcnpi:,HA=fem01:,FA=seq01:")
 
 ### Allstop, alldone
 dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=495idc:")
 # Create PV aliases so old medm screens can be used
-dbLoadRecords("$(TOP)/495idcApp/Db/allstopAliases.db")
+#dbLoadRecords("$(TOP)/databases/allstopAliases.db")
 
 ### Scan-support software
 # crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
 # 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
 putenv "SDB=$(SSCAN)/sscanApp/Db/standardScans.db"
 dbLoadRecords("$(SDB)","P=495idc:,MAXPTS1=8000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=8000")
-dbLoadRecords("$(SSCAN)/sscanApp/Db/saveData.db","P=495idc:")
-dbLoadRecords("$(SSCAN)/sscanApp/Db/scanProgress.db","P=495idc:scanProgress:")
+dbLoadRecords("$(TOP)/databases//saveData.db","P=495idc:")
+dbLoadRecords("$(TOP)/databases//scanProgress.db","P=495idc:scanProgress:")
 # Create PV aliases so old medm screens can be used
-dbLoadRecords("$(TOP)/495idcApp/Db/sscanAliases.db")
+#dbLoadRecords("$(TOP)/databases/sscanAliases.db")
 
 < cmds/user.cmd
 # Create PV aliases so old medm screens can be used
-dbLoadRecords("$(TOP)/495idcApp/Db/userAliases.db")
-dbLoadRecords("$(TOP)/495idcApp/Db/placeholderAliases.db")
+#dbLoadRecords("$(TOP)/databases/userAliases.db")
+#dbLoadRecords("$(TOP)/databases/placeholderAliases.db")
 
 # Miscellaneous PV's, such as burtResult
 dbLoadRecords("$(STD)/stdApp/Db/misc.db","P=495idc:")
