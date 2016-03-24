00b9cfa26b205fe280039f08e4a2290e1fa20ad0
prjemian <prjemian@gmail.com>
2016-03-24 16:15:48 -0500

    index and cross-reference the various .rst sections

--- src/iocdoc/reports.py
+++ src/iocdoc/reports.py
@@ -9,54 +9,65 @@ import pyRestTable
 import text_file
 
 
-def writeFile(fname, title, text):
-    f =  open(fname, 'w')
-    f.write('.. file - ' + fname)
-    f.write('\n')
-    f.write('\n')
-    f.write(mk_title(title, '='))
-    f.write('\n')
-    f.write(text)
-    f.write('\n')
-    f.write('\n' + '-'*10 + '\n'*2)
-    f.write('written: ' + str(datetime.datetime.now()))
-    f.write('\n')
-    f.close()
-    return fname
+class writeReports(object):
+    
+    def __init__(self, obj, ioc_name='IOC'):
+        self.st_cmd_object = obj
+        self.ioc_name = ioc_name
+        self.writeReports()
 
+    def writeReports(self):
+        '''report what was learned from the command file'''
+        obj = self.st_cmd_object
+        ioc_name = self.ioc_name
+        files = []
+        files.append(self.writeFile('command_sequence.rst',  ioc_name+': IOC Command sequence',              reportCommandSequence(obj.commands)))
+        files.append(self.writeFile('rtyp.rst',              ioc_name+': EPICS Records types used',          reportRTYP(obj.pv_dict)))
+        files.append(self.writeFile('command_list.rst',      ioc_name+': EPICS IOC shell commands used',     reportCommandCount(obj.commands)))
+        files.append(self.writeFile('motor_types.rst',       ioc_name+': EPICS Motor types used',            reportMotorCount(obj.pv_dict)))
+        files.append(self.writeFile('pvs.rst',               ioc_name+': Process Variables',                 reportPVs(obj.pv_dict)))
+        files.append(self.writeFile('macros.rst',            ioc_name+': Macros',                            reportSymbols(obj.env.db)))
+        files.append(self.writeFile('symbols.rst',           ioc_name+': Symbols',                           reportSymbols(obj.symbols.db)))
+        files.append(self.writeFile('databases.rst',         ioc_name+': EPICS Databases',                   reportDatabases(obj.database_list)))
+        files.append(self.writeFile('templates.rst',         ioc_name+': EPICS Templates/Substitutions',     reportDatabases(obj.template_list)))
+        files.append(self.writeFile('text_files.rst',        ioc_name+': text file cache',                   reportTextFiles()))
+    
+        # write the index.rst that coordinates all this
+        indent = ' '*3
+        title = 'IOC: ' + ioc_name
+        text = ':st.cmd file: ' + str(obj.filename)
+        text += '\n'
+        text += ':absolute path: ' + str(obj.filename_absolute)
+        text += '\n'*2
+        text += 'Contents:\n\n'
+        text += '.. toctree::\n'
+        text += indent + ':maxdepth: 1\n'
+        text += indent + ':glob:\n'
+        text += '\n'
+        for f in sorted(files):
+            nm = os.path.splitext(f)[0]
+            text += indent + nm + '\n'
+        self.writeFile('index.rst', title, text)
+    
+        # TODO: write a conf.py file
 
-def writeReports(obj, ioc_name='IOC'):
-    '''report what was learned from the command file'''
-    files = []
-    files.append(writeFile('command_sequence.rst',  ioc_name+': IOC Command sequence',              reportCommandSequence(obj.commands)))
-    files.append(writeFile('rtyp.rst',              ioc_name+': EPICS Records types used',          reportRTYP(obj.pv_dict)))
-    files.append(writeFile('command_list.rst',      ioc_name+': EPICS IOC shell commands used',     reportCommandCount(obj.commands)))
-    files.append(writeFile('motor_types.rst',       ioc_name+': EPICS Motor types used',            reportMotorCount(obj.pv_dict)))
-    files.append(writeFile('pvs.rst',               ioc_name+': Process Variables',                 reportPVs(obj.pv_dict)))
-    files.append(writeFile('macros.rst',            ioc_name+': Macros',                            reportSymbols(obj.env.db)))
-    files.append(writeFile('symbols.rst',           ioc_name+': Symbols',                           reportSymbols(obj.symbols.db)))
-    files.append(writeFile('databases.rst',         ioc_name+': EPICS Databases',                   reportDatabases(obj.database_list)))
-    files.append(writeFile('templates.rst',         ioc_name+': EPICS Templates/Substitutions',     reportDatabases(obj.template_list)))
-    files.append(writeFile('text_files.rst',        ioc_name+': text file cache',                   reportTextFiles()))
-
-    # write the index.rst that coordinates all this
-    indent = ' '*3
-    title = 'IOC: ' + ioc_name
-    text = ':st.cmd file: ' + str(obj.filename)
-    text += '\n'
-    text += ':absolute path: ' + str(obj.filename_absolute)
-    text += '\n'*2
-    text += 'Contents:\n\n'
-    text += '.. toctree::\n'
-    text += indent + ':maxdepth: 1\n'
-    text += indent + ':glob:\n'
-    text += '\n'
-    for f in sorted(files):
-        nm = os.path.splitext(f)[0]
-        text += indent + nm + '\n'
-    writeFile('index.rst', title, text)
-
-    # TODO: write a conf.py file
+    def writeFile(self, fname, title, text):
+        f =  open(fname, 'w')
+        f.write('.. file - ' + fname)
+        f.write('\n'*2)
+        f.write('.. index:: ' + self.ioc_name + ', ' + os.path.splitext(fname)[0])
+        f.write('\n'*2)
+        f.write('.. _IOC_' + self.ioc_name + '_' + os.path.splitext(fname)[0])
+        f.write('\n'*2)
+        f.write(mk_title(title, '='))
+        f.write('\n')
+        f.write(text)
+        f.write('\n')
+        f.write('\n' + '-'*10 + '\n'*2)
+        f.write('written: ' + str(datetime.datetime.now()))
+        f.write('\n')
+        f.close()
+        return fname
 
 
 def reportCmdFile(obj, ioc_name='Command File'):
