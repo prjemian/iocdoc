498569bf2a7cae417c61a4cfc25297d277b26cc9
prjemian <prjemian@gmail.com>
2016-03-24 15:44:40 -0500

    this is now much simpler

--- src/h1003.py
+++ src/h1003.py
@@ -3,20 +3,9 @@
 example use of iocdoc to document a single APS IOC
 '''
 
-import os
-#import iocdoc
-import iocdoc.command_file
-import iocdoc.reports
-from iocdoc.utils import logMessage, FileRef, strip_quotes
+import describe_ioc
+
 
 IOC_NAME = '9idcH1003'
-
 cmdFile = '/net/s9dserv/xorApps/epics/synApps_5_8/ioc/9idcH1003/iocBoot/ioc9idcH1003/st.cmd'
-owd = os.getcwd()
-os.chdir(os.path.dirname(os.path.abspath(cmdFile)))
-ref = FileRef(__file__, 0, 0, IOC_NAME)
-env = {}
-obj = iocdoc.command_file.CommandFile(None, os.path.split(cmdFile)[-1], ref, **env)
-
-os.chdir(owd)
-iocdoc.reports.reportCmdFile(obj, IOC_NAME)
+describe_ioc.describe(IOC_NAME, cmdFile)
