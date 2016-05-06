984abe4ad3e9396be8e5e4c37428e6866a6480a3
prjemian <prjemian@gmail.com>
2016-05-06 13:48:08 -0500

    restore this command-line interface to package content

--- src/iocdoc/describe_ioc.py
+++ src/iocdoc/describe_ioc.py
@@ -5,29 +5,32 @@ use iocdoc to document a single APS IOC
 '''
 
 import os
-import iocdoc.command_file
-import iocdoc.reports
-import iocdoc.utils
+import command_file
+import reports
+import utils
 
 
-def describe(ioc_name, st_cmd, output_path=None):
+def describe(ioc_name, st_cmd, output_path=None, report_file_cache=True):
     filename = os.path.abspath(st_cmd)
     path = os.path.dirname(filename)
 
     owd = os.getcwd()
     os.chdir(path)
-    ref = iocdoc.utils.FileRef(__file__, 0, 0, ioc_name)
+    ref = utils.FileRef(__file__, 0, 0, ioc_name)
     env = {}
-    iocdoc.utils.logMessage('IOC startup file: ' + st_cmd, iocdoc.utils.LOGGING_DETAIL__CERTAIN)
-    obj = iocdoc.command_file.CommandFile(None, os.path.split(filename)[-1], ref, **env)
+    utils.logMessage('IOC startup file: ' + st_cmd, utils.LOGGING_DETAIL__CERTAIN)
+    short_name = os.path.split(filename)[-1]
+    obj = command_file.CommandFile(None, short_name, ref, **env)
     
     os.chdir(owd)
     if output_path is None:
-        iocdoc.reports.reportCmdFile(obj, ioc_name)
+        reports.reportCmdFile(obj, ioc_name)
     else:
         os.chdir(output_path)
-        iocdoc.reports.writeReports(obj, ioc_name)
+        reports.writeReports(obj, ioc_name, report_file_cache)
         os.chdir(owd)
+    
+    return obj
 
 
 def get_command_line_parameters():
@@ -52,13 +55,13 @@ def get_command_line_parameters():
                         '--log_file',
                         dest='log_file', 
                         help='output file to store program logs',
-                        default=iocdoc.utils.LOG_FILE)
+                        default=utils.LOG_FILE)
     parser.add_argument('--reporting-level',
                         dest='reporting_level', 
                         help=msg, 
                         type=int, 
-                        choices=iocdoc.utils.LOGGING_DETAIL_LEVELS,
-                        default=iocdoc.utils.LOGGING_DETAIL__MEDIUM)
+                        choices=utils.LOGGING_DETAIL_LEVELS,
+                        default=utils.LOGGING_DETAIL__MEDIUM)
     return parser.parse_args()
 
 
@@ -68,8 +71,8 @@ def main():
         raise RuntimeError('file not found: ' + parms.st_cmd)
     if parms.output_dir is not None and not os.path.exists(parms.output_dir):
         raise RuntimeError('output directory not found: ' + parms.output_dir)
-    iocdoc.utils.LOG_FILE = parms.log_file
-    iocdoc.utils.LOGGING_DETAIL = parms.reporting_level
+    utils.LOG_FILE = parms.log_file
+    utils.LOGGING_DETAIL = parms.reporting_level
     describe(parms.IOC_name, parms.st_cmd, parms.output_dir)
 
 
