c3310a4fce6b5010836225219c770ea6d4936a5b
Pete R Jemian <prjemian@gmail.com>
2016-05-09 09:18:03 -0500

    document one demo IOC

---
+++ IOCs/doc_opc.py
@@ -0,0 +1,18 @@
+
+import os
+import sys
+
+sys.path.append(os.path.join('..', 'src'))
+
+import iocdoc
+
+
+def main():
+    ioc_name = 'OPC_SoftIOC'
+    st_cmd = os.path.join(ioc_name, 'demo', 'st.cmd')
+    doc_path = os.path.join('docs', ioc_name)
+    iocdoc.describe_ioc.describe(ioc_name, st_cmd, doc_path, True)
+
+
+if __name__ == '__main__':
+    main()
